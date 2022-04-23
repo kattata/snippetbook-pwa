ef627dea8e9f6f55108c55a1509854dbc6e416b4
Kasia Laniecka <kl@parkshare.dk>
2022-04-23 12:32:58 +0200

    update remix and tailwind

--- app/routes/snippets/index.jsx
+++
@@ -1,9 +0,0 @@
-export default function Snippet() {
-  return (
-    <div className="wrapper">
-      <div className="wrapper-inner flex justify-center">
-        <p className="text-slate-400 text-xs">No snippet selected.</p>
-      </div>
-    </div>
-  );
-}
