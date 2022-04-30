5967ca9da71316ee4d166771f06f962d82aa8d7a
aleksandrabaaa <55578732+aleksandrabaaa@users.noreply.github.com>
2022-04-30 13:45:26 +0200

    manisfest linked to the head

--- app/root.jsx
+++ app/root.jsx
@@ -23,19 +23,64 @@ export const links = () => [
     href: styles,
   },
   {
+    rel: "apple-touch-icon",
+    sizes: "180x180",
+    href: "/apple-touch-icon.png",
+  },
+  {
     rel: "icon",
-    type: "image/x-icon",
-    href: favicon,
+    type: "image/png",
+    sizes: "32x32",
+    href: "/favicon-32x32.png",
+  },
+  {
+    rel: "icon",
+    type: "image/png",
+    sizes: "16x16",
+    href: "/favicon-16x16.png",
+  },
+  {
+    rel: "manifest",
+    href: "/site.webmanifest",
+  },
+  {
+    rel: "mask-icon",
+    href: "/safari-pinned-tab.svg",
+    color: "#5bbad5",
   },
 ];
 
-export function meta() {
-  return {
+// export function meta() {
+//   return {
+//     charset: "utf-8",
+//     title: "SnippetBook",
+//     viewport: "width=device-width,initial-scale=1",
+//   },
+//   {
+//     name: "msapplication-TileColor",
+//     content: "#9f00a7",
+//   },
+//   {
+//     name: "theme-color",
+//     content: "#ffffff"
+//   },
+// }
+
+export const meta = () => [
+  {
     charset: "utf-8",
     title: "SnippetBook",
     viewport: "width=device-width,initial-scale=1",
-  };
-}
+  },
+  {
+    name: "msapplication-TileColor",
+    content: "#9f00a7",
+  },
+  {
+    name: "theme-color",
+    content: "#ffffff"
+  },
+];
 
 export async function loader() {
   const db = await connectDb();
