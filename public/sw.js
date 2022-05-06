e6dd2b6183a209b7d15d78e17de5a0a6bd526d55
Piotr Pospiech <55554468+Piotr20@users.noreply.github.com>
2022-05-06 14:49:21 +0200

    cached the whole page

--- public/sw.js
+++ public/sw.js
@@ -3,57 +3,111 @@ self.addEventListener("install", () => {
 });
 
 self.addEventListener("fetch", (event) => {
-  console.log(event.request);
-  event.respondWith(
-    // we will use the locales cache
-    caches.open("all").then(async (cache) => {
-      // we will run the fetch
-      let fetchResponsePromise = fetch(event.request);
-      try {
-        // try to read from cache
-        let cacheResponse = await cache.match(event.request);
-        if (cacheResponse) return cacheResponse;
-      } finally {
-        // and finally if it was not cached or after we sent the response
-        let fetchResponse = await fetchResponsePromise;
-        // we will update the cache
-        await cache.put(event.request, fetchResponse.clone());
-        // and return the response
-        return fetchResponse;
-      }
-    })
-  );
+  console.log(event.request.url);
+  if (event.request.destination === "document") {
+    event.respondWith(
+      caches.open("cacheDocument").then((cache) => {
+        return cache.match(event.request).then((cachedResponse) => {
+          const fetchedResponse = fetch(event.request).then(
+            (networkResponse) => {
+              cache.put(event.request, networkResponse.clone());
+
+              return networkResponse;
+            }
+          );
+
+          return cachedResponse || fetchedResponse;
+        });
+      })
+    );
+  }
+  if (event.request.destination === "style") {
+    event.respondWith(
+      caches.open("cachedStyles").then((cache) => {
+        return cache.match(event.request).then((cachedResponse) => {
+          const fetchedResponse = fetch(event.request).then(
+            (networkResponse) => {
+              cache.put(event.request, networkResponse.clone());
+
+              return networkResponse;
+            }
+          );
+
+          return cachedResponse || fetchedResponse;
+        });
+      })
+    );
+  }
+  if (event.request.destination === "") {
+    event.respondWith(
+      caches.open("cachedSnippets").then((cache) => {
+        return cache.match(event.request).then((cachedResponse) => {
+          const fetchedResponse = fetch(event.request).then(
+            (networkResponse) => {
+              console.log(networkResponse);
+              if (networkResponse.statusText == "OK") {
+                cache.put(event.request, networkResponse.clone());
+                return networkResponse;
+              }
+            }
+          );
+
+          return cachedResponse || fetchedResponse;
+        });
+      })
+    );
+  }
+  if (event.request.destination === "image") {
+    event.respondWith(
+      caches.open("cachedImages").then((cache) => {
+        return cache.match(event.request).then((cachedResponse) => {
+          const fetchedResponse = fetch(event.request).then(
+            (networkResponse) => {
+              cache.put(event.request, networkResponse.clone());
+
+              return networkResponse;
+            }
+          );
+
+          return cachedResponse || fetchedResponse;
+        });
+      })
+    );
+  }
+  if (event.request.destination === "script") {
+    event.respondWith(
+      caches.open("cachedScripts").then((cache) => {
+        return cache.match(event.request).then((cachedResponse) => {
+          const fetchedResponse = fetch(event.request).then(
+            (networkResponse) => {
+              cache.put(event.request, networkResponse.clone());
+
+              return networkResponse;
+            }
+          );
+
+          return cachedResponse || fetchedResponse;
+        });
+      })
+    );
+  }
+  if (event.request.destination === "manifest") {
+    event.respondWith(
+      caches.open("cachedManifest").then((cache) => {
+        return cache.match(event.request).then((cachedResponse) => {
+          const fetchedResponse = fetch(event.request).then(
+            (networkResponse) => {
+              cache.put(event.request, networkResponse.clone());
+
+              return networkResponse;
+            }
+          );
+
+          return cachedResponse || fetchedResponse;
+        });
+      })
+    );
+  } else {
+    return;
+  }
 });
-
-// self.addEventListener("fetch", (event) => {
-//   let url = new URL(event.request.url);
-//   let method = event.request.method;
-
-//   // any non GET request is ignored
-//   if (method.toLowerCase() !== "get") return;
-
-//   // If the request is for the favicons, fonts, or the built files (which are hashed in the name)
-//   if (
-//     url.pathname.startsWith("/favicons/") ||
-//     url.pathname.startsWith("/fonts/") ||
-//     url.pathname.startsWith("/build/")
-//   ) {
-//     event.respondWith(
-//       // we will open the assets cache
-//       caches.open("assets").then(async (cache) => {
-//         // if the request is cached we will use the cache
-//         let cacheResponse = await cache.match(event.request);
-//         if (cacheResponse) return cacheResponse;
-
-//         // if it's not cached we will run the fetch, cache it and return it
-//         // this way the next time this asset it's needed it will load from the cache
-//         let fetchResponse = await fetch(event.request);
-//         cache.put(event.request, fetchResponse.clone());
-
-//         return fetchResponse;
-//       })
-//     );
-//   }
-
-//   return;
-// });
