a3384392425b686b31e46fe0e5004ae32744209d
Piotr Pospiech <55554468+Piotr20@users.noreply.github.com>
2022-05-10 23:59:00 +0200

    caching splited

--- public/sw.js
+++ public/sw.js
@@ -1,35 +1,56 @@
 self.addEventListener("install", () => {
-  console.log("I work bitches");
+  console.log("Serwice worker installed");
 });
 
 self.addEventListener("fetch", (event) => {
-  console.log(event.request.url);
-  if (event.request.destination === "") {
-    // Check if this is a navigation request
-
+  const requestTarget = event.request.url;
+  if (
+    event.request.destination === "" &&
+    (requestTarget.includes("_data=routes") || requestTarget.includes("_data=root"))
+  ) {
     // Open the cache
     event.respondWith(
-      caches.open("snippets-cached").then(async (cache) => {
-        // Go to the network first
-        return fetch(event.request.url)
-          .then((fetchedResponse) => {
-            cache.put(event.request, fetchedResponse.clone());
+      caches
+        .open(`${requestTarget.includes("_data=routes") === true ? "snippets-cached" : "folders-cached"}`)
+        .then(async (cache) => {
+          // Go to the network first
+          return fetch(event.request.url)
+            .then((fetchedResponse) => {
+              cache.put(event.request, fetchedResponse.clone());
 
-            return fetchedResponse;
-          })
-          .catch(() => {
-            // If the network is unavailable, get
-            return cache.match(event.request.url);
-          });
+              return fetchedResponse;
+            })
+            .catch(() => {
+              // If the network is unavailable, get
+              return cache.match(event.request.url);
+            });
+        })
+    );
+  } else if (event.request.destination != "") {
+    event.respondWith(
+      caches.open(`${event.request.destination}-cached`).then(async (cache) => {
+        return cache.match(event.request).then((cachedResponse) => {
+          const fetchedResponse = fetch(event.request)
+            .then((networkResponse) => {
+              if (networkResponse.ok == true) {
+                cache.put(event.request, networkResponse.clone());
+                return networkResponse;
+              }
+            })
+            .catch((error) => {
+              return cachedResponse;
+            });
+
+          return cachedResponse || fetchedResponse;
+        });
       })
     );
   } else {
     event.respondWith(
-      caches.open(`${event.request.destination}-cached`).then((cache) => {
+      caches.open(`others-cached`).then(async (cache) => {
         return cache.match(event.request).then((cachedResponse) => {
           const fetchedResponse = fetch(event.request)
             .then((networkResponse) => {
-              console.log(networkResponse);
               if (networkResponse.ok == true) {
                 cache.put(event.request, networkResponse.clone());
                 return networkResponse;
